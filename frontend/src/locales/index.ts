a3d5ac7df9201caf70e97628ca09023874b52c72
dongwlin <dongwlin@foxmail.com>
2025-12-26 00:14:23 +0800

    feat: add Traditional Chinese language support

--- frontend/src/locales/index.ts
+++ frontend/src/locales/index.ts
@@ -2,17 +2,19 @@
 
 import { createI18n } from 'vue-i18n'
 import zhCN from './zh-CN.json'
+import zhTW from './zh-TW.json'
 import enUS from './en-US.json'
 
 const messages = {
   'zh-CN': zhCN,
+  'zh-TW': zhTW,
   'en-US': enUS,
 }
 
 type MessageSchema = typeof enUS
 
-const i18n = createI18n<[MessageSchema], 'zh-CN' | 'en-US'>({
-  legacy: false, 
+const i18n = createI18n<[MessageSchema], 'zh-CN' | 'zh-TW' | 'en-US'>({
+  legacy: false,
   locale: 'zh-CN',
   fallbackLocale: 'zh-CN',
   messages,
